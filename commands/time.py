f61a9c2179e38f7e52858031d4444113c314dbfb
Shiroko <85297739+Shiroko253@users.noreply.github.com>
2026-06-17 18:14:27 +0800

    Implement Time Cog with maintenance message
    Added a Time Cog with a slash command that informs users the command is currently unavailable.

--- commands/time.py
+++ commands/time.py
@@ -1,5 +1,23 @@
 import discord
 from discord.ext import commands
+
+class Time(commands.Cog):
+    def __init__(self, bot: discord.Bot):
+        self.bot = bot
+
+    @commands.slash_command(name="time", description="暫時無法使用該指令")
+    async def join(self, ctx: discord.ApplicationContext):
+        embed = discord.Embed(
+            title="🚧 指令維護中",
+            description="很抱歉暫時無法使用該指令，目前還在製作和維護中，請稍後等待。",
+            color=discord.Color.red()
+        )
+        embed.set_footer(text="很抱歉無法使用")
+        await ctx.respond(embed=embed, ephemeral=True)
+
+def setup(bot: discord.Bot):
+    bot.add_cog(Time(bot))import discord
+from discord.ext import commands
 import time
 import logging
 
